9ce50203b8ef8193bb9b3abe8d7423b2f66a8396
Ivan Smirnov <i.s.smirnov@gmail.com>
2022-01-16 00:11:51 +0000

    Initial version of dtype_from_type_descriptor()

--- numpy-type-desc/src/desc.rs
+++ numpy-type-desc/src/desc.rs
@@ -1,21 +1,36 @@
-use std::fmt::{self, Debug};
 use std::mem;
-use std::ops::Deref;
 use std::os::raw;
+use std::ptr;
 
-use pyo3::{ffi::Py_DECREF, PyResult, Python};
+use pyo3::{
+    exceptions::{PyMemoryError, PyValueError},
+    ffi::{Py_DECREF, Py_XDECREF},
+    types::{PyDict, PyTuple},
+    AsPyPointer, IntoPy, IntoPyPointer, Py, PyErr, PyResult, Python,
+};
 
-use numpy::npyffi::{NPY_TYPES, PY_ARRAY_API};
+use numpy::npyffi::{PyArray_Descr, NPY_TYPES, PY_ARRAY_API};
 use numpy::PyArrayDescr;
 
 use pyo3_type_desc::{
-    ArrayDescriptor, ComplexSize, Element, Endian, FloatSize, IntegerSize, RecordDescriptor,
-    Scalar as BaseScalar, ScalarDescriptor, Signedness, TypeDescriptor,
+    ArrayDescriptor, ComplexSize, Endian, FloatSize, IntegerSize, RecordDescriptor,
+    Scalar as BaseScalar, Signedness, TypeDescriptor,
 };
 
 use crate::datetime::DatetimeUnit;
 use crate::element::Scalar;
-use crate::npyffi::{PyArray_DatetimeDTypeMetaData, PyArray_DatetimeMetaData, NPY_BYTEORDER_CHAR};
+use crate::npyffi::{
+    PyArray_DatetimeDTypeMetaData, PyArray_DatetimeMetaData, PyArray_malloc, NPY_ALIGNED_STRUCT,
+    NPY_BYTEORDER_CHAR, NPY_FROM_FIELDS, NPY_NEEDS_PYAPI,
+};
+
+#[inline]
+unsafe fn pyarray_descr_replace(descr: &mut *mut PyArray_Descr) {
+    // an equivalent of PyArray_DESCR_REPLACE macro
+    let new = PY_ARRAY_API.PyArray_DescrNew(*descr);
+    Py_XDECREF(*descr as _);
+    *descr = new;
+}
 
 fn npy_int_type_lookup<T, T0, T1, T2>(npy_types: [NPY_TYPES; 3]) -> NPY_TYPES {
     // `npy_common.h` defines the integer aliases. In order, it checks:
@@ -29,177 +44,233 @@ fn npy_int_type_lookup<T, T0, T1, T2>(npy_types: [NPY_TYPES; 3]) -> NPY_TYPES {
     }
 }
 
-#[derive(Clone, PartialEq, Eq, Hash)]
-pub struct NumpyDescriptor(TypeDescriptor<Scalar>);
-
-impl<S: ScalarDescriptor + Into<Scalar>> From<TypeDescriptor<S>> for NumpyDescriptor {
-    fn from(desc: TypeDescriptor<S>) -> Self {
-        Self(desc.map(|scalar| scalar.clone().into()))
-    }
-}
-
-impl Deref for NumpyDescriptor {
-    type Target = TypeDescriptor<Scalar>;
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-
-impl Debug for NumpyDescriptor {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{:?}", self.0)
-    }
-}
-
-impl NumpyDescriptor {
-    pub fn of<T: Element<Scalar>>() -> Self {
-        T::type_descriptor().into()
-    }
-
-    pub fn get_dtype_args(&self) -> DtypeArgs<'_> {
-        type B = BaseScalar;
-        type D<'a> = DtypeArgs<'a>;
-        type S = Signedness;
-        type I = IntegerSize;
-        type F = FloatSize;
-        type C = ComplexSize;
-        type N = NPY_TYPES;
-
-        match &self.0 {
-            TypeDescriptor::Object => D::typenum(N::NPY_OBJECT),
-            TypeDescriptor::Scalar(scalar) => {
-                match *scalar {
-                    Scalar::Base(scalar) => match scalar {
-                        B::Bool => D::typenum(N::NPY_BOOL),
-                        B::Char(S::Signed) => D::typenum(N::NPY_BYTE),
-                        B::Char(S::Unsigned) => D::typenum(N::NPY_UBYTE),
-                        B::Integer(I::Two, S::Signed, endian) => {
-                            D::typenum_endian(N::NPY_SHORT, endian)
-                        }
-                        B::Integer(I::Two, S::Unsigned, endian) => {
-                            D::typenum_endian(N::NPY_USHORT, endian)
-                        }
-                        B::Integer(I::Four, S::Signed, endian) => {
-                            D::typenum_endian(
-                                npy_int_type_lookup::<i32, raw::c_long, raw::c_int, raw::c_short>(
-                                    [N::NPY_LONG, N::NPY_INT, N::NPY_SHORT],
-                                ),
-                                endian,
-                            )
-                        }
-                        B::Integer(I::Four, S::Unsigned, endian) => {
-                            D::typenum_endian(
-                                npy_int_type_lookup::<u32, raw::c_ulong, raw::c_uint, raw::c_ushort>(
-                                    [N::NPY_ULONG, N::NPY_UINT, N::NPY_USHORT],
-                                ),
-                                endian,
-                            )
-                        }
-                        B::Integer(I::Eight, S::Signed, endian) => {
-                            D::typenum_endian(
-                                npy_int_type_lookup::<i64, raw::c_long, raw::c_longlong, raw::c_int>(
-                                    [N::NPY_LONG, N::NPY_LONGLONG, N::NPY_INT],
-                                ),
-                                endian,
-                            )
-                        }
-                        B::Integer(I::Eight, S::Unsigned, endian) => D::typenum_endian(
-                            npy_int_type_lookup::<u64, raw::c_ulong, raw::c_ulonglong, raw::c_uint>(
-                                [N::NPY_ULONG, N::NPY_ULONGLONG, N::NPY_UINT],
-                            ),
-                            endian,
-                        ),
-                        B::Float(F::Two, endian) => D::typenum_endian(N::NPY_HALF, endian),
-                        B::Float(F::Four, endian) => D::typenum_endian(N::NPY_FLOAT, endian),
-                        B::Float(F::Eight, endian) => D::typenum_endian(N::NPY_DOUBLE, endian),
-                        B::Complex(C::Four, endian) => D::typenum_endian(N::NPY_CFLOAT, endian),
-                        B::Complex(C::Eight, endian) => D::typenum_endian(N::NPY_CDOUBLE, endian),
-                        B::Bytes(size) => D::typenum_flexible(N::NPY_STRING, size),
-                        B::Ucs4(size) => D::typenum_flexible(N::NPY_UNICODE, size),
-                    },
-                    Scalar::Datetime(unit) => D::typenum_datetimeunit(N::NPY_DATETIME, unit),
-                    Scalar::Timedelta(unit) => D::typenum_datetimeunit(N::NPY_TIMEDELTA, unit),
-                }
-            }
-            TypeDescriptor::Array(arr) => D::array(&arr),
-            TypeDescriptor::Record(rec) => D::record(&rec),
+#[inline]
+fn checked_elsize(
+    size: usize, itemsize: usize, err: impl FnOnce() -> PyErr,
+) -> PyResult<raw::c_int> {
+    if let Some(elsize) = size.checked_mul(itemsize) {
+        if elsize <= raw::c_int::MAX as usize {
+            return Ok(elsize as _);
         }
     }
+    Err(err())
 }
 
-#[derive(Debug, Clone, Copy)]
-pub enum TypenumExtra {
-    Flexible(usize),
-    Endian(Endian),
-    DatetimeUnit(DatetimeUnit),
+unsafe fn create_dtype_object(py: Python) -> PyResult<*mut PyArray_Descr> {
+    let dtype = PY_ARRAY_API.PyArray_DescrFromType(NPY_TYPES::NPY_OBJECT as _);
+    if dtype.is_null() {
+        return Err(PyErr::fetch(py));
+    }
+    Ok(dtype)
 }
 
-#[derive(Debug, Clone)]
-pub enum DtypeArgs<'a> {
-    Typenum(NPY_TYPES, Option<TypenumExtra>),
-    Array(&'a ArrayDescriptor<Scalar>),
-    Record(&'a RecordDescriptor<Scalar>),
-}
-
-impl<'a> DtypeArgs<'a> {
-    pub fn typenum(typenum: NPY_TYPES) -> Self {
-        Self::Typenum(typenum, None)
+unsafe fn create_dtype_scalar(py: Python, scalar: &Scalar) -> PyResult<*mut PyArray_Descr> {
+    #[derive(Clone, Copy)]
+    pub enum Quirks {
+        None,
+        Flexible(usize, usize),
+        Endian(Endian),
+        DatetimeUnit(DatetimeUnit),
     }
 
-    pub fn typenum_endian(typenum: NPY_TYPES, endian: Endian) -> Self {
-        Self::Typenum(typenum, Some(TypenumExtra::Endian(endian)))
+    type B = BaseScalar;
+    type S = Signedness;
+    type I = IntegerSize;
+    type F = FloatSize;
+    type C = ComplexSize;
+    type N = NPY_TYPES;
+    type Q = Quirks;
+
+    let (npy_type, quirks) = match *scalar {
+        Scalar::Base(scalar) => match scalar {
+            B::Bool => (N::NPY_BOOL, Q::None),
+            B::Char(S::Signed) => (N::NPY_BYTE, Q::None),
+            B::Char(S::Unsigned) => (N::NPY_UBYTE, Q::None),
+            B::Integer(I::Two, S::Signed, endian) => (N::NPY_SHORT, Q::Endian(endian)),
+            B::Integer(I::Two, S::Unsigned, endian) => (N::NPY_USHORT, Q::Endian(endian)),
+            B::Integer(I::Four, S::Signed, endian) => (
+                npy_int_type_lookup::<i32, raw::c_long, raw::c_int, raw::c_short>([
+                    N::NPY_LONG,
+                    N::NPY_INT,
+                    N::NPY_SHORT,
+                ]),
+                Q::Endian(endian),
+            ),
+            B::Integer(I::Four, S::Unsigned, endian) => (
+                npy_int_type_lookup::<u32, raw::c_ulong, raw::c_uint, raw::c_ushort>([
+                    N::NPY_ULONG,
+                    N::NPY_UINT,
+                    N::NPY_USHORT,
+                ]),
+                Q::Endian(endian),
+            ),
+            B::Integer(I::Eight, S::Signed, endian) => (
+                npy_int_type_lookup::<i64, raw::c_long, raw::c_longlong, raw::c_int>([
+                    N::NPY_LONG,
+                    N::NPY_LONGLONG,
+                    N::NPY_INT,
+                ]),
+                Q::Endian(endian),
+            ),
+            B::Integer(I::Eight, S::Unsigned, endian) => (
+                npy_int_type_lookup::<u64, raw::c_ulong, raw::c_ulonglong, raw::c_uint>([
+                    N::NPY_ULONG,
+                    N::NPY_ULONGLONG,
+                    N::NPY_UINT,
+                ]),
+                Q::Endian(endian),
+            ),
+            B::Float(F::Two, endian) => (N::NPY_HALF, Q::Endian(endian)),
+            B::Float(F::Four, endian) => (N::NPY_FLOAT, Q::Endian(endian)),
+            B::Float(F::Eight, endian) => (N::NPY_DOUBLE, Q::Endian(endian)),
+            B::Complex(C::Four, endian) => (N::NPY_CFLOAT, Q::Endian(endian)),
+            B::Complex(C::Eight, endian) => (N::NPY_CDOUBLE, Q::Endian(endian)),
+            B::Bytes(size) => (N::NPY_STRING, Q::Flexible(size, 1)),
+            B::Ucs4(size) => (N::NPY_UNICODE, Q::Flexible(size, 4)), // TODO: check if correct
+        },
+        Scalar::Datetime(unit) => (N::NPY_DATETIME, Q::DatetimeUnit(unit)),
+        Scalar::Timedelta(unit) => (N::NPY_TIMEDELTA, Q::DatetimeUnit(unit)),
+    };
+
+    let mut dtype = PY_ARRAY_API.PyArray_DescrFromType(npy_type as _);
+    if dtype.is_null() {
+        return Err(PyErr::fetch(py));
     }
 
-    pub fn typenum_flexible(typenum: NPY_TYPES, elsize: usize) -> Self {
-        Self::Typenum(typenum, Some(TypenumExtra::Flexible(elsize)))
-    }
-
-    pub fn typenum_datetimeunit(typenum: NPY_TYPES, unit: DatetimeUnit) -> Self {
-        Self::Typenum(typenum, Some(TypenumExtra::DatetimeUnit(unit)))
-    }
-
-    pub fn array(arr: &'a ArrayDescriptor<Scalar>) -> Self {
-        Self::Array(arr)
-    }
-
-    pub fn record(rec: &'a RecordDescriptor<Scalar>) -> Self {
-        Self::Record(rec)
-    }
-
-    pub unsafe fn into_dtype(self, py: Python) -> PyResult<&PyArrayDescr> {
-        let dtype = match self {
-            Self::Typenum(typenum, extra) => {
-                let mut dtype = PY_ARRAY_API.PyArray_DescrFromType(typenum as _);
-                // TODO: check if null?
-                match extra {
-                    Some(TypenumExtra::Flexible(elsize)) => {
-                        // TODO: checked cast? (hypothetical c_int overflow)
-                        (*dtype).elsize = elsize as _;
-                    }
-                    Some(TypenumExtra::Endian(endian)) if endian.is_swapped() => {
-                        // we bypass PyArray_DescrNewByteorder since it has lots of extra logic
-                        // that we don't need (e.g. recursively setting endianness for subarrays
-                        // and record type fields)
-                        let dtype_swapped = PY_ARRAY_API.PyArray_DescrNew(dtype);
-                        // TODO: check if null?
-                        (*dtype_swapped).byteorder = NPY_BYTEORDER_CHAR::NPY_OPPBYTE as _;
-                        Py_DECREF(dtype as _);
-                        dtype = dtype_swapped;
-                    }
-                    Some(TypenumExtra::DatetimeUnit(unit)) => {
-                        // an equivalent of create_datetime_dtype()
-                        let meta =
-                            PyArray_DatetimeMetaData { base: unit.into_npy_datetimeunit(), num: 1 };
-                        let dtype_meta = (*dtype).c_metadata as *mut PyArray_DatetimeDTypeMetaData;
-                        (*dtype_meta).meta = meta;
-                    }
-                    _ => {}
-                }
-                dtype
+    match quirks {
+        Quirks::Flexible(size, width) => {
+            (*dtype).elsize = checked_elsize(size, width, || {
+                PyValueError::new_err(
+                    "invalid size of a string type: itemsize does not fit into a C int.",
+                )
+            })?;
+        }
+        Quirks::Endian(endian) if endian.is_swapped() => {
+            // we bypass PyArray_DescrNewByteorder since it has lots of extra logic
+            // that we don't need (e.g. recursively setting endianness for subarrays
+            // and record type fields)
+            pyarray_descr_replace(&mut dtype);
+            if dtype.is_null() {
+                return Err(PyErr::fetch(py));
             }
-            _ => todo!(),
-        };
-        Ok(py.from_owned_ptr(dtype as _))
+            (*dtype).byteorder = NPY_BYTEORDER_CHAR::NPY_OPPBYTE as _;
+        }
+        Quirks::DatetimeUnit(unit) => {
+            // reference: create_datetime_dtype() in descriptor.c
+            let meta = PyArray_DatetimeMetaData { base: unit.into_npy_datetimeunit(), num: 1 };
+            // pyarray_descr_replace is not required here, we can just mutate the dtype
+            let dtype_meta = (*dtype).c_metadata as *mut PyArray_DatetimeDTypeMetaData;
+            (*dtype_meta).meta = meta;
+        }
+        _ => {}
     }
+
+    Ok(dtype)
+}
+
+unsafe fn create_dtype_array(
+    py: Python, arr: &ArrayDescriptor<Scalar>,
+) -> PyResult<*mut PyArray_Descr> {
+    // reference: _convert_from_tuple() in descriptor.c
+    let ArrayDescriptor { desc, shape } = arr;
+
+    let nbytes = checked_elsize(arr.itemsize(), arr.size(), || {
+        PyValueError::new_err(
+            "invalid shape in fixed-type tuple: dtype size in bytes must fit into a C int.",
+        )
+    })?;
+
+    let base = create_dtype_any(py, &desc)?;
+
+    let dtype = PY_ARRAY_API.PyArray_DescrFromType(NPY_TYPES::NPY_VOID as _);
+    if dtype.is_null() {
+        return Err(PyErr::fetch(py));
+    }
+
+    (*dtype).subarray = PyArray_malloc(mem::size_of::<PyArray_Descr>()) as _;
+    if (*dtype).subarray.is_null() {
+        Py_DECREF(dtype as _);
+        return Err(PyMemoryError::new_err(""));
+    }
+
+    (*dtype).elsize = nbytes as _;
+    (*dtype).flags = (*base).flags;
+    (*dtype).alignment = (*base).alignment;
+    (*(*dtype).subarray).base = base;
+    Py_XDECREF((*dtype).fields);
+    Py_XDECREF((*dtype).names);
+    (*dtype).fields = ptr::null_mut() as _;
+    (*dtype).names = ptr::null_mut() as _;
+    (*(*dtype).subarray).shape = PyTuple::new(py, shape.iter().copied()).into_ptr();
+
+    Ok(dtype)
+}
+
+unsafe fn create_dtype_record(
+    py: Python, rec: &RecordDescriptor<Scalar>,
+) -> PyResult<*mut PyArray_Descr> {
+    // reference: _convert_from_dict() in descriptor.c
+    let RecordDescriptor { fields, itemsize, alignment } = rec;
+
+    let mut flags = NPY_NEEDS_PYAPI;
+    if rec.is_aligned() {
+        // we will take the initiative to always mark aligned structs as 'aligned' because we can
+        flags |= NPY_ALIGNED_STRUCT;
+    }
+
+    // TODO: validate layout:
+    // - itemsize is a multiple of alignment
+    // - no repeating field names
+    // - no invalid (empty,\0,\n) field names
+    // - no field overlaps (numpy only checks it for objects)
+    // - optional: non-zero size?
+    // - optional: at least on field?
+
+    let names = rec.fill_names().map(|name| name.to_string()).collect::<Vec<_>>();
+    let names_tuple = PyTuple::new(py, &names);
+
+    let fields_dict = PyDict::new(py);
+    for (field, name) in fields.iter().zip(&names) {
+        let dtype: Py<PyArrayDescr> = dtype_from_type_descriptor(py, &field.desc)?.into_py(py);
+        flags |= (*(dtype.as_ptr() as *mut PyArray_Descr)).flags & NPY_FROM_FIELDS;
+        let field_item: Py<PyTuple> = (dtype, field.offset).into_py(py);
+        if fields_dict.contains(&name).unwrap_or(false) {
+            return Err(PyValueError::new_err(format!(
+                "name {:?} already used as a name or title",
+                name
+            )));
+        }
+        fields_dict.set_item(name, field_item)?;
+    }
+
+    let dtype = PY_ARRAY_API.PyArray_DescrFromType(NPY_TYPES::NPY_VOID as _);
+    if dtype.is_null() {
+        return Err(PyErr::fetch(py));
+    }
+
+    (*dtype).fields = fields_dict.into_ptr();
+    (*dtype).names = names_tuple.into_ptr();
+    (*dtype).flags = flags;
+    // TODO: numpy assigns alignment only in 'aligned' mode, should we just always set it?
+    (*dtype).alignment = alignment.unwrap_or(1) as _;
+    (*dtype).elsize = *itemsize as _;
+
+    Ok(dtype)
+}
+
+unsafe fn create_dtype_any(
+    py: Python, desc: &TypeDescriptor<Scalar>,
+) -> PyResult<*mut PyArray_Descr> {
+    match desc {
+        TypeDescriptor::Object => create_dtype_object(py),
+        TypeDescriptor::Scalar(scalar) => create_dtype_scalar(py, scalar),
+        TypeDescriptor::Array(arr) => create_dtype_array(py, &arr),
+        TypeDescriptor::Record(rec) => create_dtype_record(py, &rec),
+    }
+}
+
+pub fn dtype_from_type_descriptor<'py>(
+    py: Python<'py>, desc: &TypeDescriptor<Scalar>,
+) -> PyResult<&'py PyArrayDescr> {
+    Ok(unsafe { py.from_owned_ptr(create_dtype_any(py, desc)? as _) })
 }
