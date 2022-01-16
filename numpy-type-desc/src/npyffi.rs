ab77b2c5933c199983a4d296c69a159fd1620cc5
Ivan Smirnov <i.s.smirnov@gmail.com>
2022-01-16 00:10:41 +0000

    Add dtype flags to npyffi

--- numpy-type-desc/src/npyffi.rs
+++ numpy-type-desc/src/npyffi.rs
@@ -1,6 +1,7 @@
 #![allow(non_camel_case_types)]
 
 pub use self::array::*;
+pub use self::flags::*;
 pub use self::objects::*;
 pub use self::types::*;
 
@@ -9,6 +10,30 @@ pub mod array {
     pub use pyo3::ffi::PyMem_RawFree as PyArray_free;
     pub use pyo3::ffi::PyMem_RawMalloc as PyArray_malloc;
     pub use pyo3::ffi::PyMem_RawRealloc as PyArray_realloc;
+}
+
+pub mod flags {
+    use numpy::npyffi::npy_char;
+
+    pub const NPY_ITEM_REFCOUNT: npy_char = 0x01;
+    pub const NPY_ITEM_HASOBJECT: npy_char = 0x01;
+    pub const NPY_LIST_PICKLE: npy_char = 0x02;
+    pub const NPY_ITEM_IS_POINTER: npy_char = 0x04;
+    pub const NPY_NEEDS_INIT: npy_char = 0x08;
+    pub const NPY_NEEDS_PYAPI: npy_char = 0x10;
+    pub const NPY_USE_GETITEM: npy_char = 0x20;
+    pub const NPY_USE_SETITEM: npy_char = 0x40;
+    pub const NPY_ALIGNED_STRUCT: npy_char = -128; // 0x80;
+
+    pub const NPY_FROM_FIELDS: npy_char =
+        NPY_NEEDS_INIT | NPY_LIST_PICKLE | NPY_ITEM_REFCOUNT | NPY_NEEDS_PYAPI;
+
+    pub const NPY_OBJECT_DTYPE_FLAGS: npy_char = NPY_LIST_PICKLE
+        | NPY_USE_GETITEM
+        | NPY_ITEM_IS_POINTER
+        | NPY_ITEM_REFCOUNT
+        | NPY_NEEDS_INIT
+        | NPY_NEEDS_PYAPI;
 }
 
 pub mod types {
