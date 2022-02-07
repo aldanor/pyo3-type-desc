bb3a678b8dc0ef177436e54d222d95ee7f98606a
Ivan Smirnov <i.s.smirnov@gmail.com>
2022-02-07 16:28:43 +0000

    Update the test for #[derive(Record)]

--- numpy-type-desc/tests/test_derive.rs
+++ numpy-type-desc/tests/test_derive.rs
@@ -1,6 +1,6 @@
-use numpy_type_desc::{td, units, Element as _, Record, Timedelta64};
+use numpy_type_desc::{dtype, td, units, Element as _, Record, Timedelta64};
 
-use pyo3::PyObject;
+use pyo3::{py_run, PyObject};
 
 #[test]
 fn test_struct_1() {
@@ -16,7 +16,7 @@ fn test_struct_1() {
 #[test]
 fn test_struct_2() {
     #[derive(Clone, Record)]
-    #[repr(packed)]
+    #[repr(C)]
     struct Foo<T, const N: usize> {
         x: [[T; N]; 3],
         y: (PyObject, Timedelta64<units::Nanosecond>),
@@ -24,6 +24,25 @@ fn test_struct_2() {
 
     assert_eq!(
         Foo::<i64, 2>::type_descriptor(),
-        td!({"x":0 => [(3, 2); <i64], "y":48 => {0 => object, 8 => timedelta64[ns] [16, 8]} [64, 1]})
+        td!({"x":0 => [(3, 2); <i64], "y":48 => {0 => O, 8 => m8[ns] [16, 8]} [64, 8]})
     );
+
+    pyo3::Python::with_gil(|py| {
+        let dt = dtype::<Foo<i64, 2>>(py).unwrap();
+        py_run!(
+            py,
+            dt,
+            r#"
+            args = dict(
+                names=['x', 'y'],
+                offsets=[0, 48],
+                formats=[('<i8', (3, 2)),[('f0', 'O'), ('f1', '<m8[ns]')]],
+                itemsize=64,
+                aligned=True,
+            )
+            dtype = __import__('numpy').dtype(args)
+            assert dtype == dt
+        "#
+        );
+    });
 }
